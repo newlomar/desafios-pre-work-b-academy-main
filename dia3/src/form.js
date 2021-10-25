385ed7ce2e2d0aef74ea04a877d377107fa48e1d
Newton Lomar <newtonlomar16@gmail.com>
2021-10-25 07:26:28 -0300

    progredindo com o exercico 2

--- dia3/src/form.js
+++ dia3/src/form.js
@@ -1,6 +1,9 @@
-const form = document.querySelector('[data-js="form"]')
+import './style.css'
 
-form.addEventListener('input', (e) => {
+const form = document.querySelector('[data-js="form"]')
+const input = document.querySelector('[data-js="input"]')
+
+input.addEventListener('input', (e) => {
 
   const nome = e.target.value.toLowerCase()
   const arraydoNome = nome.split(' ')
@@ -19,3 +22,43 @@ form.addEventListener('input', (e) => {
   e.target.value = arrayNovoNome.join(' ')
 
 })
+
+const select = document.createElement('select')
+select.multiple = true
+
+const colors = [
+  {
+    name: 'red',
+    hex: '#f00'
+  },
+  {
+    name: 'green',
+    hex: '#0f0'
+  },
+  {
+    name: 'blue',
+    hex: '#00f'
+  },
+  {
+    name: 'yellow',
+    hex: '#ff0'
+  },
+  {
+    name: 'purple',
+    hex: '#f0f'
+  }
+]
+
+const options = colors.map((cor) => {
+
+  const option = document.createElement('option')
+
+  option.value = cor.hex
+  option.textContent = cor.name
+  select.insertAdjacentElement('beforeend', option)
+})
+
+select.addEventListener('click', (e) => {
+  console.log([...select.options].filter(item => item.selected))
+})
+form.insertAdjacentElement('beforeend', select)
