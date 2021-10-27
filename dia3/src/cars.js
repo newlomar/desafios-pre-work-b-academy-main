edcd4677239ae403d4530b68cb0a7e28907be9de
Newton Lomar <newtonlomar16@gmail.com>
2021-10-27 13:04:06 -0300

    progredindo

--- dia3/src/cars.js
+++ dia3/src/cars.js
@@ -11,9 +11,10 @@ form_carros.addEventListener('submit', (e) => {
 
   const tdImg = document.createElement('td')
   const img = document.createElement('img')
-  img.attributes.src = `${dados['image_url'].value}`
-  console.log(img.attributes.src)
+  img.src = `${dados['image_url'].value}`
   tdImg.insertAdjacentElement('beforeend', img)
+  img.style.height = '100px'
+  img.style.width = '100px'
 
   const tdMarca = document.createElement('td')
   tdMarca.textContent = `${dados['marca_modelo'].value}`
