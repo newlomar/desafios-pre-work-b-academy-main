4d466b148787610f7e76c5bf51a0e30c60dcda07
Newton Lomar <newtonlomar16@gmail.com>
2021-10-27 09:54:40 -0300

    progredindo

---
+++ dia3/src/cars.js
@@ -0,0 +1,35 @@
+const form_carros = document.querySelector('[data-js="form_carros"]')
+const table = document.querySelector('[data-js="table"]')
+
+form_carros.addEventListener('submit', (e) => {
+
+  e.preventDefault()
+
+  const dados = e.target.elements
+
+  const tr = document.createElement('tr')
+
+  const tdImg = document.createElement('td')
+  const img = document.createElement('img')
+  img.attributes.src = `${dados['image_url'].value}`
+  console.log(img.attributes.src)
+  tdImg.insertAdjacentElement('beforeend', img)
+
+  const tdMarca = document.createElement('td')
+  tdMarca.textContent = `${dados['marca_modelo'].value}`
+
+  const tdAno = document.createElement('td')
+  tdAno.textContent = dados.ano.value
+
+  const tdCor = document.createElement('td')
+  tdCor.textContent = dados.cor.value
+
+
+  tr.insertAdjacentElement('beforeend', tdImg)
+  tr.insertAdjacentElement('beforeend', tdMarca)
+  tr.insertAdjacentElement('beforeend', tdAno)
+  tr.insertAdjacentElement('beforeend', tdCor)
+
+  table.insertAdjacentElement('beforeend', tr)
+  form_carros.reset()
+})
